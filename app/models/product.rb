42dc0dec17d90865afc1634ce23e698b14f95fe4
Clay26 <5711623+Clay26@users.noreply.github.com>
2024-02-13 03:11:16 +0000

    Add validations and break form into separate views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    validates :name, presence: true
+    validates :description, presence: true, length: { minimum: 10 }
+    validates :price, presence: true, numericality: { greater_than: 0.00 }
 end
